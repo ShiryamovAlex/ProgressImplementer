c89bf7f990cb14bed7875d03c93123ab3db53382
Ширямов А.И <shiryamovai@tomskasu.ru>
2020-07-03 22:29:15 +0700

    #2 - Добавлен проект для тестов и тест с запуском окна. В .gitignore добавлен игнор на папку [Pp]ackages.

---
+++ src/ProgressImplementer.Tests/UI/Windows/ProgressWindowTests.cs
@@ -0,0 +1,22 @@
+﻿namespace ProgressImplementer.Tests.UI.Windows
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using ProgressImplementer.UI.Windows;
+
+    [TestClass]
+    public class ProgressWindowTests
+    {
+        /// <summary>
+        /// Тест окна для просмотра окна без запуска приложения.
+        /// Игнор указан, чтобы тест не завис при запуске.
+        /// </summary>
+        [TestMethod]
+        [Ignore("Тест с окном")]
+        public void ShowWindowTest()
+        {
+            var window = new ProgressWindow();
+            window.ShowDialog();
+        }
+    }
+}
